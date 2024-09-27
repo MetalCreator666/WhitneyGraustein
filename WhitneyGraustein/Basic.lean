107781ee57fbd354d2b92f925e2b95da8f1b299d
MetalCreator666 <57985847+MetalCreator666@users.noreply.github.com>
2024-09-27 15:00:19 +0200

    Initial setup sphere-eversion

--- WhitneyGraustein/Basic.lean
+++ WhitneyGraustein/Basic.lean
@@ -1,1 +1,5 @@
-def hello := "world"
+import SphereEversion.Global.Immersion
+
+-- Some superficial checks to see if it works
+#check immersionRel_open
+#check immersionRel_open_ample
--- lakefile.lean
+++ lakefile.lean
@@ -4,6 +4,8 @@ open Lake DSL
 package "WhitneyGraustein" where
   -- add package configuration options here
 
+require SphereEversion from git "https://github.com/leanprover-community/sphere-eversion"@"master"
+
 lean_lib «WhitneyGraustein» where
   -- add library configuration options here
 
